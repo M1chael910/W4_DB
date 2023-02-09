3e8a9835dde0725fb4b5572508a941d5c46aa0ef
M1chael910 <mpmurphybusiness@gmail.com>
2023-02-09 17:56:02 -0600

    second commit

--- W4_Q1.cpp
+++ W4_Q1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+string grade = "";
+string calcGrade(int grade);
+// This initializes a function with an integer parameter.
+// Allows us to put the code for the function below the main function
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Enter a percentage score 1-100 from a recent assignment" << endl;
+    calcGrade(0);
     return 0;
+}
+
+string calcGrade(int grade) {
+    cout << "Hello";
 }
