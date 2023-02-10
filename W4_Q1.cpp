bf74bb31b6713e37f97a02819f0e78287d2b816d
M1chael910 <mpmurphybusiness@gmail.com>
2023-02-10 17:36:01 -0600

    Finished core program

--- W4_Q1.cpp
+++ W4_Q1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string calcGrade(int grade);
 // This initializes a function with an integer parameter.
 // Allows us to put the code for the function below the main function
-
+// Source:: https://www.w3schools.com/cpp/cpp_functions.asp
 int main() {
     int grade = 0;
     cout << "Enter a percentage score 1-100 from a recent assignment" << endl;
@@ -25,8 +25,8 @@ string calcGrade(int grade) {
     } else if (grade >= 80.0 && grade <= 89.9) {
         return "B";
     } else if (grade >= 90.0 && grade <= 100.0) {
-        return "B";
+        return "A";
     } else {
-        return "I'm confused";
+        return "Not a valid score";
     }
 }
--- W4_Q2.cpp
+++ W4_Q2.cpp
@@ -1,6 +1,55 @@
+/*
+Prompt the user to enter a score (1-100)
+
+Enter a Function and using switch / case determine and output the Letter grade
+
+Repeat for 3 scores.
+
+Calculate the average score and then the Final Letter Grade
+ */
+
+
+
 #include <iostream>
 
+using namespace std;
+
+
+string calcGrade(int grade);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int score1, score2, score3;
+    double average = 0.0;
+    cout << "Enter a score 1-100." << endl;
+    cin >> score1;
+    string Grade1 = calcGrade(score1);
+    cout << Grade1 << endl;
+    cout << "Enter a score 1-100." << endl;
+    cin >> score2;
+    string Grade2 = calcGrade(score2);
+    cout << Grade2 << endl;
+    cout << "Enter a score 1-100." << endl;
+    cin >> score3;
+    string Grade3 = calcGrade(score3);
+    cout << Grade3 << endl;
+    average = (score1 + score2 + score3) / 3;
+    cout << "Your average score is a " << calcGrade(average) << "." << endl;
     return 0;
 }
+
+string calcGrade(int grade) {
+    int simplifiedGrade = grade / 10; // Allows us to deal with fewer cases in the switch statement
+    switch (simplifiedGrade) {
+        case 6:
+            return "D";
+        case 7:
+            return "C";
+        case 8:
+            return "B";
+        case 9:
+            return "A";
+        default:
+            return "F";
+    }
+}
+
