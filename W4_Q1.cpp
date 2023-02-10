cea410f1a51f61e7f73eac984f7872542ced7c6f
M1chael910 <mpmurphybusiness@gmail.com>
2023-02-09 18:23:44 -0600

    Finished core program

--- W4_Q1.cpp
+++ W4_Q1.cpp
@@ -2,17 +2,31 @@
 
 using namespace std;
 
-string grade = "";
+int grade = 0;
 string calcGrade(int grade);
 // This initializes a function with an integer parameter.
 // Allows us to put the code for the function below the main function
 
 int main() {
     cout << "Enter a percentage score 1-100 from a recent assignment" << endl;
-    calcGrade(0);
+    cin >> grade;
+    string calculatedGrade = calcGrade(grade);
+    cout << "Your grade is a " << calculatedGrade << "."<< endl;
     return 0;
 }
 
 string calcGrade(int grade) {
-    cout << "Hello";
+    if (grade <= 59.9) {
+        return "F";
+    } else if (grade >= 60.0 && grade <= 69.9) {
+        return "D";
+    } else if (grade >= 70.0 && grade <= 79.9) {
+        return "C";
+    } else if (grade >= 80.0 && grade <= 89.9) {
+        return "B";
+    } else if (grade >= 90.0 && grade <= 100.0) {
+        return "B";
+    } else {
+        return "I'm confused";
+    }
 }
